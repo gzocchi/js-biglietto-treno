6f004f796e6f5fea5c40427fd9b238c0713ba2b0
gzocchi <z.giorgio.z@gmail.com>
2021-04-27 13:44:31 +0200

    creazione file progetto

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+/* Calcolare il prezzo totale del viaggio:
+- prezzo del biglietto definito in base ai km (0.21 € al km)
+- sconto del 20% per i minorenni
+- sconto del 40% per gli over 65.*/
+
+
+//Output con prezzo finale massimo due decimali
